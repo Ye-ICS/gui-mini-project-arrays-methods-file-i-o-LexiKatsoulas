655d9eef7851b2881fee684c6c8f4f1dc7d98515
LexiKatsoulas <katsoulasl@limestone.on.ca>
2025-12-01 17:33:19 -0500

    Add methods and import file

--- App.java
+++ App.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -11,12 +13,17 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-
+import javafx.stage.FileChooser;
+import java.io.File;
+import javafx.scene.text.Text;
 /**
  * Template JavaFX application.
  */
-public class App extends Application {
-    
+public class App extends Application{
+    VBox contentBox;
+    HBox buttonBox;
+    TextField addTasks;
+    Text fileText;
     public static void main(String[] args) {
         launch(args);
     }
@@ -31,7 +38,10 @@ public class App extends Application {
         introBox.setAlignment(Pos.TOP_CENTER);
 
         // has the content of the lists
-        VBox contentBox = new VBox();
+        contentBox = new VBox();
+        buttonBox = new HBox();
+        buttonBox.setAlignment(Pos.CENTER);
+        
 
         Label welcome = new Label();
         welcome.setText("Welcome to the To-Do List!");
@@ -39,84 +49,73 @@ public class App extends Application {
         Label label = new Label();
         label.setText("Please enter your tasks: ");
 
-        TextField addTasks = new TextField();
+        addTasks = new TextField();
         addTasks.setPromptText("type here");
         addTasks.setMaxWidth(250);
         Button addTasksBtn = new Button("Add");
-
+        Button clearTasksBtn = new Button("Clear Tasks");
+        Button fileBtn = new Button("Import list from file");
 
 
         // organize compents
         introBox.getChildren().addAll(welcome, label, addTasks, addTasksBtn);
+        buttonBox.getChildren().addAll(clearTasksBtn, fileBtn);
         contentBox.getChildren().addAll();
-        presetTunesBox.getChildren().addAll(introBox, contentBox);
-
+        presetTunesBox.getChildren().addAll(introBox, contentBox, buttonBox);
 
         // Reactions 
-        addTasksBtn.setOnAction(event -> {
-            String text = addTasks.getText();
-            addTasks.clear();
-            HBox toDo = new HBox();
-            Label toDoItem = new Label(" " + text);
-            toDoItem.setStyle("-fx-font-size: 13");
-            CheckBox checkBox = new CheckBox();
-            checkBox.setMaxSize(12, 13);
-            toDo.getChildren().addAll(checkBox, toDoItem);
-            contentBox.getChildren().add(toDo);
-        });
-
-
+        addTasksBtn.setOnAction(event -> task());
+        clearTasksBtn.setOnAction(event -> clear());
+        fileBtn.setOnAction(event -> importFile());
 
         // set scene and display stage
         Scene scene = new Scene(presetTunesBox, 400, 300);
         stage.setScene(scene);
         stage.show();
 
+    }
+    // Method for adding tasks
+    void task() {
+        String text = addTasks.getText();
+        addTasks.clear();
+        CheckBox checkBox = new CheckBox();
+        HBox toDo = new HBox();
+        Label toDoItem = new Label(" " + text);
+        toDoItem.setStyle("-fx-font-size: 13");
+        checkBox.setMaxSize(12, 13);
 
+        checkBox.setOnAction(checkboxEvent -> {
+            toDo.setDisable(true);
+        });
+        toDo.getChildren().addAll(checkBox, toDoItem);
+        contentBox.getChildren().add(toDo);
+    }
+    void clear() {
+        contentBox.getChildren().clear();
+    }
+    void importFile() {
+        File file = new File("list.txt");
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+                if (!line.isEmpty()) {
 
+                    CheckBox checkBox = new CheckBox();
+                    HBox toDo = new HBox();
+                    Label toDoItem = new Label(" " + line);
+                    toDoItem.setStyle("-fx-font-size: 13");
+                    checkBox.setMaxSize(12, 13);
 
+                    checkBox.setOnAction(checkboxEvent -> {
+                        toDo.setDisable(true);
+                    });
 
-    //     VBox contentBox = new VBox();
-    //     contentBox.setAlignment(Pos.CENTER);
-
-    //     Label promptLabel = new Label();
-    //     promptLabel.setText("TO-DO LIST");
-
-    //     TextField thoughtsBox = new TextField();
-    //     thoughtsBox.setMaxWidth(150);
-    //     thoughtsBox.setPromptText("type here");
-        
-    //     messageBox = new VBox();
-
-    //     Button submissionBtn = new Button();
-    //     submissionBtn.setText("Submit");
-
-    //     // Set up reactions (aka callbacks).
-    //     // submissionBtn.setOnAction(event -> onSubmitThought(thoughtsBox, messageBox));
-
-    //     // Add components to the content box.
-    //     contentBox.getChildren().add(promptLabel);
-    //     contentBox.getChildren().add(thoughtsBox);
-    //     contentBox.getChildren().add(submissionBtn);
-    //     contentBox.getChildren().add(messageBox);
-
-    //     // Set up the window and display it.
-    //     Scene scene = new Scene(contentBox, 300, 200);
-    //     stage.setScene(scene);
-    //     stage.setTitle("To-Do List App");
-    //     stage.show();
-    // }
-
-    // /**
-    //  * Handle the submission of a thought.
-    //  * @param inputBox  The TextField where the user types their thought.
-    //  * @param outputBox The TextArea where the submitted thoughts are displayed.
-    //  */
-    // void onSubmitThought(TextField inputBox, VBox outputBox) {
-    //     String text = inputBox.getText();
-    //     inputBox.clear();
-    //     outputBox.appendText( text + "\n");
-    //     Button checkBtn = new Button("O");
-    //     messageBox.getChildren().add(checkBtn);
+                    toDo.getChildren().addAll(checkBox, toDoItem);
+                    contentBox.getChildren().add(toDo);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            return;
+        }
     }
 }
